f1ea6016532ed266a9d2f3f3af57e3ea36dbc2bf
Saggins <win32prog@gmail.com>
2021-03-03 10:38:02 -0800

    helper functions planned

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //import {v4 as uuidv4} from 'uuid';
 let uuid = require('uuid');
 
-
+ 
 const express = require('express')
 const bodyParser = require('body-parser')
 
@@ -10,7 +10,13 @@ const port = 3000
 app.use(bodyParser.json)
 
 // Have the correct body parser
-
+/**
+ * TODO:Create DB with the schema
+ * 
+ * DB is for storing ML stuff
+ * 
+ * uuid          procssedMessage
+ */
 /** 
  * SQL SCHEMA 
  * uuid    message    time
@@ -20,9 +26,7 @@ app.use(bodyParser.json)
 /**
  * Sends to DB
  * Assigns UUID
- * 
- * 
- * 
+ * Gives Timestamp and sends to DB
  */
 app.post('/add' , (req, res) => {
     if (req.body.uuid == "") {
@@ -33,21 +37,46 @@ app.post('/add' , (req, res) => {
 
     res.send(req.body.uuid)
 })
+/**
+ * Needs UUID 
+ * returns all the prossed data
+ * TODO:Gives prossesd data
+ */
+app.get('/get', (req, res) => {
+
+})
+
+/**
+ * TODO: Indicates that User is finished with session and deletes it
+ */
+app.delete('/delete', (req, res) => {
+
+})
 
 function connect () {
     const password = ""
 }
 
-
-
 // PART 2 WENDSDAYYY
 /**
- * sends response of machine learning (mysql)
+ * TODO:ML asks for list of jobs. Returns a json fild with 
+ * UUID : [blob1, blob2, blob3 ...]
  */
-app.get ('/notes', (req, res) => {
+app.get ('/jobs', (req, res) => {
 
 })
 
+/**
+ * TODO:ML gives all the prossesd blobs in json
+ * 
+ * UUID : Message
+ */
+app.post( '/jobs', (req, res) => {
+
+})
+
+
+//TODO: Helper funtions to database Cleanupa
 app.listen(port, () => {
 
 })
