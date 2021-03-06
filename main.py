f96c2652fd99b7dbb955ae7893aeb32d17c20009
keshprad <keshav.r.pradeep@gmail.com>
2021-03-06 12:31:21 -0800

    sql for joining and starting meeting

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ db = mysql.connector.connect(
 )
 mycursor = db.cursor()
 
+
 # Setting up sql - Creating Tables
 def create_tables():
     unprocessed = "CREATE TABLE IF NOT EXISTS unprocessed (meetingid char(38) NOT NULL, uid char(38) NOT NULL, " \
@@ -19,31 +20,44 @@ def create_tables():
                   "CURRENT_TIMESTAMP, PRIMARY KEY (meetingid, time)) DEFAULT CHARSET=utf8; "
     processed = "CREATE TABLE IF NOT EXISTS processed (meetingid char(38) NOT NULL, notes LONGTEXT NOT NULL, " \
                 "date DATE NOT NULL, PRIMARY KEY (meetingid)) DEFAULT CHARSET=utf8; "
-    meetings = "CREATE TABLE IF NOT EXISTS meetings (meetingid char(38) NOT NULL, host_uid char(38), PRIMARY KEY (" \
+    meetings = "CREATE TABLE IF NOT EXISTS meetings (meetingid char(38) NOT NULL, host_uid char(38) NOT NULL, PRIMARY KEY (" \
                "meetingid)) DEFAULT CHARSET=utf8; "
     for e in (unprocessed, processed, meetings):
         mycursor.execute(e)
     print("Tables are ready!")
 create_tables()
 
-add_owner= ("")
-# Client makes post request with a dictionary that has "user" key
+
+# Client makes post request with a dictionary that has "name" key
 # Server responds with User
 @app.post("/start")
 def start_meeting(user: User):
     user = user.dict()
-    user['meetingid'] = uuid.uuid4()
-    user['uid'] = uuid.uuid4()
+    user['meetingid'], user['uid'] = str(uuid.uuid4()), str(uuid.uuid4())
+
+    # Create meeting in meetings db
+    sql = "INSERT INTO meetings (meetingid, host_uid) VALUES (%s, %s)"
+    vals = (user['meetingid'], user['uid'])
+    mycursor.execute(sql, vals)
+    db.commit()
+
     return user
 
-add_user = ("")
-# Client makes post request with a dictionary that has "meetingid" & "user" key
+
+# Client makes post request with a dictionary that has "meetingid" & "name" key
 # Server responds with User
 @app.post("/join")
 def join_meeting(user: User):
     user = user.dict()
-    user["uid"] = uuid.uuid4()
-    return user
+
+    # Check if meeting exists
+    sql = "SELECT * FROM meetings where meetingid = '%s'" % user["meetingid"]
+    mycursor.execute(sql)
+
+    if mycursor.fetchone() is not None:
+        user["uid"] = str(uuid.uuid4())
+        return user
+    return "Meeting does not exist. Please check with host to ensure your invite link is correct."
 
 
 @app.post("/add")
