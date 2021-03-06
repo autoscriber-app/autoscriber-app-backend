9d89ed6dfe8a74b68fbd3cd3765b694ec85117a7
keshprad <keshav.r.pradeep@gmail.com>
2021-03-06 11:36:38 -0800

    sql connection. remove node files.

---
+++ basemodels.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class User(BaseModel):
+    meetingid: str = ''
+    uid: str = ''
+    name: str = ''
+
+
+class TranscriptEntry(BaseModel):
+    user: User
+    dialogue: str
+    timestamp: int
--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-from typing import Optional
 from fastapi import FastAPI
-from pydantic import BaseModel
+import mysql.connector
 import uuid
+from basemodels import User, TranscriptEntry
 
 app = FastAPI()
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=open("sql_pass").read(),
+    database="autoscriber_app",
+)
+mycursor = db.cursor()
+
+# Setting up sql - Creating Tables
+def create_tables():
+    unprocessed = "CREATE TABLE IF NOT EXISTS unprocessed (meetingid char(38) NOT NULL, uid char(38) NOT NULL, " \
+                  "name varchar(255) NOT NULL, message LONGTEXT NOT NULL, time TIMESTAMP NOT NULL DEFAULT " \
+                  "CURRENT_TIMESTAMP, PRIMARY KEY (meetingid, time)) DEFAULT CHARSET=utf8; "
+    processed = "CREATE TABLE IF NOT EXISTS processed (meetingid char(38) NOT NULL, notes LONGTEXT NOT NULL, " \
+                "date DATE NOT NULL, PRIMARY KEY (meetingid)) DEFAULT CHARSET=utf8; "
+    meetings = "CREATE TABLE IF NOT EXISTS meetings (meetingid char(38) NOT NULL, host_uid char(38), PRIMARY KEY (" \
+               "meetingid)) DEFAULT CHARSET=utf8; "
+    for e in (unprocessed, processed, meetings):
+        mycursor.execute(e)
+    print("Tables are ready!")
+create_tables()
 
 
-class TranscriptEntry(BaseModel):
-    meetingid: str
-    uuid: str
-    name: str
-    dialogue: str
-    timestamp: int
-
-
-class User(BaseModel):
-    meetingid: str
-    uuid: str
-
-
-@app.get("/")
-def root():
-    return {'key': 'value'}
-
-
+# Client makes post request with a dictionary that has "user" key
+# Server responds with User
 @app.post("/start")
-def start_meeting(meet: str):
-    uid = uuid.uuid4()
-    return uid
+def start_meeting(user: User):
+    user = user.dict()
+    user['meetingid'] = uuid.uuid4()
+    user['uid'] = uuid.uuid4()
+    return user
 
 
+# Client makes post request with a dictionary that has "meetingid" & "user" key
+# Server responds with User
 @app.post("/join")
-def join_meeting(meet: str):
-    uid = uuid.uuid4()
-    return uid
+def join_meeting(user: User):
+    user = user.dict()
+    user["uid"] = uuid.uuid4()
+    return user
 
 
 @app.post("/add")
