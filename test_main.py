e55cb19d0548c2878f0cda2ef3e7fee52b481235
saggins <win32prog@gmail.com>
2021-03-31 23:09:55 -0700

    Broken Test

--- test_main.py
+++ test_main.py
@@ -1,8 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, params
 from fastapi.testclient import TestClient
+import mysql
 import pytest
+import json
 import main
+client = TestClient(main.app)
 
-def test_meetingIDTest():
+USER = ""
+PASSWORD = ""
+db = mysql.connector.connect(
+    host="localhost",
+    user=USER,
+    password=PASSWORD,
+    database="testScriber"
+)
+
+mycursor = db.cursor()
+
+def isMeetingIdValid(id):
+    return len(id) == 10
+
+def test_meetingId():
     newMeetingid = main.uuidCreator()
-    assert len(newMeetingid) == 10
+    assert isMeetingIdValid(newMeetingid)
+
+def test_hostEndpoint () :
+    response = client.get("/host")
+    user = json.loads(response.json())
+
+    sql_findMeetingidQuery = "SELECT meeting_id FROM meetings WHERE meeting_id=%s"
+    mycursor.execute(sql_findMeetingidQuery, params= (user['meeting_id']))
+    db.commit()
+    sql_meetingid = mycursor.get_row()
+    sql_findUUIDQuery = "SELECT meeting_id FROM meetings WHERE meeting_id=%s"
+    mycursor.execute(sql_findUUIDQuery, params= (user['user_id']))
+    db.commit()
+    sql_uuid = mycursor.get_row()
+
+
+    assert isMeetingIdValid(user["meeting_id"])
+    assert response.json() == {
+        "meeting_id" : str(sql_meetingid),
+        "uid" : str(sql_uuid),
+    }
+    
