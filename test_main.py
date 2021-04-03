41e68bcad5a9ffda46d742cba93a0d3f3a089b5c
saggins <win32prog@gmail.com>
2021-04-02 23:09:12 -0700

    Imported Tets

--- test_main.py
+++ test_main.py
@@ -3,6 +3,31 @@ from fastapi.testclient import TestClient
 import pytest
 import main
 
-def test_meetingIDTest():
+
+def isMeetingIdValid(id):
+    return len(id) == 10
+
+def test_meetingId():
     newMeetingid = main.uuidCreator()
-    assert len(newMeetingid) == 10
+    assert isMeetingIdValid(newMeetingid)
+
+def test_hostEndpoint () :
+    response = client.get("/host")
+    user = json.loads(response.json())
+
+    sql_findMeetingidQuery = "SELECT meeting_id FROM meetings WHERE meeting_id=%s"
+    mycursor.execute(sql_findMeetingidQuery, params= (user['meeting_id']))
+    db.commit()
+    sql_meetingid = mycursor.get_row()
+    sql_findUUIDQuery = "SELECT meeting_id FROM meetings WHERE meeting_id=%s"
+    mycursor.execute(sql_findUUIDQuery, params= (user['user_id']))
+    db.commit()
+    sql_uuid = mycursor.get_row()
+
+
+    assert isMeetingIdValid(user["meeting_id"])
+    assert response.json() == {
+        "meeting_id" : str(sql_meetingid),
+        "uid" : str(sql_uuid),
+    }
+    
