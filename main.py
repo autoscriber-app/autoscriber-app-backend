d53e6436f3c7470d7dd9631831c063d026a27068
saggins <win32prog@gmail.com>
2021-04-07 13:28:12 -0700

    Fixed CORS?

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ import uvicorn
 
 
 app = FastAPI()
+origins = ["http://localhost:8000", "http://127.0.0.1:8000"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
