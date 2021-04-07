514ff35ac660c800a83c291acc2126ef72f27c8e
saggins <win32prog@gmail.com>
2021-04-07 13:29:06 -0700

    Need to specfiy the things

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ import uvicorn
 
 
 app = FastAPI()
+origins = ["https://autoscriber.sagg.in:8000/"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
